dab0bba8bc480f83710ea8687a1b948411f33fd7
Ahon-pan <Sdp_518@126.com>
2017-08-11 11:38:04 +0800

    change js

--- user-js.js
+++ user-js.js
@@ -1,6 +1,7 @@
-
+$(function(){
         var div = document.querySelectorAll('div')[0];
         div.onclick = function() {
             $("html,body").animate({scrollTop:0},2000);
         }
+})
     
